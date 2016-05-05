68d96137508ad198d52f9224f03012023eb52823
Padmaja Ragavendra <pragave@l-sb8m11kg8w-m.local>
2016-05-04 23:00:19 -0700

    Initial Commit

---
+++ tipapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipapp
+//
+//  Created by Padmaja Ragavendra on 5/4/16.
+//  Copyright © 2016 Padmaja Ragavendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
